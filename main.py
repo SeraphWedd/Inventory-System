63da18574f54fcf6508064a5a0d1a6869fe0c55c
J. E. Venturillo <seraphwedd18@gmail.com>
2022-04-05 22:36:15 +0800

    Completed Implementation of Add_User function
    Added add_user function, fiters for username and password, check for duplicate accounts, and database updating.

--- main.py
+++ main.py
@@ -188,10 +188,11 @@ class LoginWindow():
 
         #Buttons
         self.log_btn = Button(bottomlvl, text='Login',
-                              font=('arial', 14), width=15, command=self.login)
+                              font=('arial', 14), width=15,
+                              command=self.login)
         self.log_btn.grid(row=0)
         self.lbl_warn = Label(bottomlvl, text="",
-                              font=('arial', 14), bd=10,
+                              font=('arial', 12), bd=10,
                               bg="#77ddff")
         self.lbl_warn.grid(row=1)
         self.form.bind('<Return>', self.login)
@@ -263,6 +264,11 @@ class AccountManagement():
 
         self.username = StringVar()
         self.password = StringVar()
+        self.conf_pass = StringVar()
+        self.authority = StringVar()
+        self.lastname = StringVar()
+        self.firstname = StringVar()
+        
         self.add_new()
         self.form.transient(self.master)
         self.form.grab_set()
@@ -274,19 +280,39 @@ class AccountManagement():
                          height=self.h*.7, bd=0,
                          bg="#77ddff", relief=SOLID)
         toplevel.pack(side=TOP, pady=5)
+        midlevel = Frame(self.form, width=self.w,
+                         height=self.h*.15, bd=0,
+                         bg="#77ddff", relief=SOLID)
+        midlevel.pack(side=TOP, pady=5)
         bottomlvl = Frame(self.form, width=self.w,
                           height=self.h*.3, bd=0,
                          bg="#77ddff",  relief=SOLID)
         bottomlvl.pack(side=TOP, pady=5)
         #Labels
         self.lbl_user = Label(toplevel, text='Username:',
-                              font=('arial', 14), bd=10,
+                              font=('arial', 14), bd=5,
                               bg="#77ddff")
         self.lbl_user.grid(row=0, column=0)
         self.lbl_pass = Label(toplevel, text="Password:",
-                              font=('arial', 14), bd=10,
+                              font=('arial', 14), bd=5,
                               bg="#77ddff")
         self.lbl_pass.grid(row=1, column=0)
+        self.lbl_conf = Label(toplevel, text="Confirm Password:",
+                              font=('arial', 14), bd=5,
+                              bg="#77ddff")
+        self.lbl_conf.grid(row=2, column=0)
+        self.lbl_auth = Label(toplevel, text="Authority:",
+                              font=('arial', 14), bd=5,
+                              bg="#77ddff")
+        self.lbl_auth.grid(row=3, column=0)
+        self.lbl_last = Label(toplevel, text="Last Name:",
+                              font=('arial', 14), bd=5,
+                              bg="#77ddff")
+        self.lbl_last.grid(row=4, column=0)
+        self.lbl_first = Label(toplevel, text="First Name:",
+                              font=('arial', 14), bd=5,
+                              bg="#77ddff")
+        self.lbl_first.grid(row=5, column=0)
         #Entry boxes
         self.user = Entry(toplevel, textvariable=self.username,
                           font=('arial', 14), width=18)
@@ -294,16 +320,94 @@ class AccountManagement():
         self.pwd = Entry(toplevel, textvariable=self.password,
                          font=('arial', 14), width=18, show="*")
         self.pwd.grid(row=1, column=3)
+        self.conf_pwd = Entry(toplevel, textvariable=self.conf_pass,
+                         font=('arial', 14), width=18, show="*")
+        self.conf_pwd.grid(row=2, column=3)
+        self.auth = Entry(toplevel, textvariable=self.authority,
+                         font=('arial', 14), width=18)
+        self.auth.grid(row=3, column=3)
+        self.last = Entry(toplevel, textvariable=self.lastname,
+                         font=('arial', 14), width=18)
+        self.last.grid(row=4, column=3)
+        self.first = Entry(toplevel, textvariable=self.firstname,
+                         font=('arial', 14), width=18)
+        self.first.grid(row=5, column=3)
 
         #Buttons
-        self.log_btn = Button(bottomlvl, text='Login',
-                              font=('arial', 14), width=15, command=self.login)
-        self.log_btn.grid(row=0)
+        self.add_btn = Button(midlevel, text='Add',
+                              font=('arial', 14), width=15,
+                              command=self.add_user)
+        self.add_btn.grid(row=0, column=0)
+        self.reset_btn = Button(midlevel, text='Reset',
+                              font=('arial', 14), width=15,
+                              command=self.reset_form)
+        self.reset_btn.grid(row=0, column=3)
         self.lbl_warn = Label(bottomlvl, text="",
-                              font=('arial', 14), bd=10,
+                              font=('arial', 12), bd=5,
                               bg="#77ddff")
-        self.lbl_warn.grid(row=1)
-        self.form.bind('<Return>', self.login)
+        self.lbl_warn.grid(row=0)
+        self.form.bind('<Return>', self.add_user)
+
+    def reset_form(self):
+        self.username.set("")
+        self.password.set("")
+        self.conf_pass.set("")
+        self.authority.set("")
+        self.lastname.set("")
+        self.firstname.set("")
+        self.lbl_warn.config(text="")
+
+    def add_user(self):
+        self.database()
+        
+        if (self.username.get() == "" or
+            self.password.get() == "" or
+            self.conf_pass.get() == "" or
+            self.authority.get() == "" or
+            self.lastname.get() == "" or
+            self.firstname.get() == ""):
+            self.lbl_warn.config(text="Please enter the required values!",
+                                 fg="red")
+        elif (self.password.get() != self.conf_pass.get()):
+            self.lbl_warn.config(text="Passwords do not match!",
+                                 fg="red")
+        elif len(self.password.get()) < 6:
+            self.lbl_warn.config(text="Password should be at least "+
+                                 "6 characters long!",
+                                 fg="red")
+        elif set(self.username.get().lower()).difference(
+            self.checker) != set():
+            self.lbl_warn.config(
+                text="Username should be alphanumeric!",
+                fg="red")
+        else:
+            #Search for username from database
+            self.cursor.execute(
+                "SELECT * FROM `Users` WHERE `username` = ?",
+                (self.username.get(),)
+                )
+            if self.cursor.fetchone() is not None: #Username overlap!
+                self.lbl_warn.config(text="Username already exists!",
+                                     fg="red")
+            else: #Save user as new
+                self.cursor.execute(
+                "INSERT INTO `Users` (username, password, "+
+                "authority, lastname, firstname) "+
+                f"VALUES('{self.username.get()}', "+
+                f"'{self.password.get()}', '{self.authority.get()}', "+
+                f"'{self.lastname.get()}', '{self.firstname.get()}')"
+                )
+                self.conn.commit()
+                tkMessageBox.showinfo(
+                    "New User successfully added!",
+                    "Welcome to the DOST-MIMAROPA RSTL - Inventory "+
+                    f"System user: {self.username.get()}!"
+                    )
+                #Reset the variables
+                self.form.destroy()
+                
+        self.cursor.close()
+        self.conn.close()
         
         
 if __name__ == "__main__":
